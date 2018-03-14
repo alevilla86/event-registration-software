74150e9e3f84233be7aa26969ac717dcd4a62030
Alejandro Villalobos <avillalobos@jetsonsys.com>
2018-03-14 15:52:00 -0600

    Added copyright

--- ers-core/src/main/java/com/ers/core/service/CategoryService.java
+++ ers-core/src/main/java/com/ers/core/service/CategoryService.java
@@ -1,3 +1,6 @@
+/*
+ * Copyright (C) 2018-2019 ERS - Alejandro Villalobos Hernandez (alevilla86@hotmail.com). All rights reserved.
+ */
 package com.ers.core.service;
 
 import com.ers.core.dao.CategoryDao;
